09925dafc9ddf0d8cbfa0f6a6624bfd3f3b93a1d
map220v <map220v300@gmail.com>
2024-09-03 12:04:19 +0000

    ASoC: Add cs35l43 codec driver

--- drivers/firmware/cirrus/cs_dsp.c
+++ drivers/firmware/cirrus/cs_dsp.c
@@ -2171,7 +2171,7 @@ out:
 	return ret;
 }
 
-static int cs_dsp_load_coeff(struct cs_dsp *dsp, const struct firmware *firmware,
+int cs_dsp_load_coeff(struct cs_dsp *dsp, const struct firmware *firmware,
 			     const char *file)
 {
 	LIST_HEAD(buf_list);
@@ -2385,6 +2385,7 @@ out_fw:
 
 	return ret;
 }
+EXPORT_SYMBOL_GPL(cs_dsp_load_coeff);
 
 static int cs_dsp_create_name(struct cs_dsp *dsp)
 {
